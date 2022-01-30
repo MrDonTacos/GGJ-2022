224bf64c2c9344f4a1e3a08ba122177fab7d7581
Nicktte <87045373+Nicktte@users.noreply.github.com>
2022-01-30 14:43:27 -0800

    Adding Player ship movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    //public Projectile laserPrefab;
+    public System.Action killed;
+    public bool laserActive { get; private set; }
+
+    private void Update()
+    {
+        Vector3 position = transform.position;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            //position.x -= speed * Time.deltaTime;
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            //position.x += speed * Time.deltaTime;
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= Vector3.up * speed * Time.deltaTime;
+        }
+
+        /*Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        // Clamp the position of the character so they do not go out of bounds
+        position.x = Mathf.Clamp(position.x, leftEdge.x, rightEdge.x);
+        transform.position = position;
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            Shoot();
+        }*/
+    }
+
+    private void Shoot()
+    {
+        // Only one laser can be active at a given time so first check that
+        // there is not already an active laser
+        if (!laserActive)
+        {
+            laserActive = true;
+
+            //Projectile laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+            //laser.destroyed += OnLaserDestroyed;
+        }
+    }
+
+    /*private void OnLaserDestroyed(Projectile laser)
+    {
+        laserActive = false;
+    }*/
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Missile") ||
+            other.gameObject.layer == LayerMask.NameToLayer("Invader"))
+        {
+            if (killed != null) {
+                killed.Invoke();
+            }
+        }
+    }
+
+}
